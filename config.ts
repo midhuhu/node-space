be2fc1acbdde0d6d0f3216be25ce1c4d0ad4e8a9
zlq <midhuhu@163.com>
2024-03-26 16:37:45 +0800

    [mod]  update mysql

--- config.ts
+++ config.ts
@@ -3,11 +3,10 @@ const config = {
     mysql: {
         host: 'localhost',
         user: 'root',
-        password: 'root',
-        database: 'test',
-        port: 3306
-    }
-}
-
+        password: 'root123456',
+        database: 'saas_basic',
+        port: 3306,
+    },
+};
 
 export default config;

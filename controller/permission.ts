e62cbb7ee45e44d6acc6513aa08b4d7fb3c4b9ac
zlq <midhuhu@163.com>
2024-04-03 16:13:51 +0800

    [mod] 权限按钮列表的CRUD

--- controller/permission.ts
+++ controller/permission.ts
@@ -3,13 +3,16 @@
  * @Author          : zlq midhuhu@163.com
  * @Description:    : 页面权限按钮管理
  * @Date            : 2024-03-28 16:40:37
- * @LastEditTime    : 2024-04-03 15:19:07
+ * @LastEditTime    : 2024-04-03 16:11:09
  * @Copyright (c) 2024 by zhijiasoft.
  */
 import { Response } from 'express';
 import { dbService } from '../app';
 import { ReqExpress } from '../types';
 import BaseResult from '../types/base-result';
+import { nanoid } from 'nanoid';
+import snowFlake from '../utils/snow-flake';
+import dayjs from 'dayjs';
 
 class PermissionController {
     /**
@@ -38,7 +41,6 @@ class PermissionController {
      * @param res
      */
     queryPermissions = async (req: ReqExpress, res: Response) => {
-        console.log(111111);
         try {
             const menuId = req.query.menuId;
             const search = req.query.search;
@@ -53,8 +55,63 @@ class PermissionController {
                 page,
                 pageSize,
             );
-            if (result.data.length === 0) return res.send(BaseResult.success('暂无数据'));
             return res.send(BaseResult.success({ data: result.data, total: result.total }));
+        } catch (error) {
+            return res.send(BaseResult.fail('失败:' + error));
+        }
+    };
+    /**
+     * 创建数据
+     */
+    create = async (req: ReqExpress, res: Response) => {
+        try {
+            const { name, code, menuId } = req.body;
+            const result = await dbService.insert('saas_button', {
+                id: snowFlake.nextId(),
+                created_at: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+                updated_at: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+                name,
+                code,
+                menu_id: menuId,
+            });
+            return res.send(BaseResult.success(result));
+        } catch (error) {
+            return res.send(BaseResult.fail('失败:' + error));
+        }
+    };
+    /**
+     * 更新数据
+     */
+    update = async (req: ReqExpress, res: Response) => {
+        try {
+            const { id, name, code, menuId } = req.body;
+            await dbService.update(
+                'saas_button',
+                {
+                    name,
+                    code,
+                    menu_id: menuId,
+                    updated_at: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+                },
+                'id = ?',
+                [id],
+            );
+            return res.send(BaseResult.success('更新成功'));
+        } catch (error) {
+            return res.send(BaseResult.fail('失败:' + error));
+        }
+    };
+    /**
+     * 删除数据
+     * body:{ids:['xxx','xxx']}
+     */
+    delete = async (req: ReqExpress, res: Response) => {
+        try {
+            const { ids } = req.body;
+            ids.forEach(async (item: any) => {
+                await dbService.delete('saas_button', 'id = ?', [item]);
+            });
+            return res.send(BaseResult.success('删除成功'));
         } catch (error) {
             return res.send(BaseResult.fail('失败:' + error));
         }
--- routes/permission.ts
+++ routes/permission.ts
@@ -3,7 +3,7 @@
  * @Author          : zlq midhuhu@163.com
  * @Description:    : 权限按钮设置
  * @Date            : 2024-04-03 13:53:05
- * @LastEditTime    : 2024-04-03 14:21:37
+ * @LastEditTime    : 2024-04-03 16:09:07
  * @Copyright (c) 2024 by zhijiasoft.
  */
 
@@ -12,5 +12,8 @@ const router = express.Router();
 import { permissionController } from '../controller';
 
 router.get('/saas/api/v1/button/list', permissionController.queryPermissions);
+router.post('/saas/api/v1/button/create', permissionController.create);
+router.post('/saas/api/v1/button/update', permissionController.update);
+router.post('/saas/api/v1/button/delete', permissionController.delete);
 
 export default router;

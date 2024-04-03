e7921fb125008d657b4b4e2af849f85eabf90f0b
zlq <midhuhu@163.com>
2024-04-03 11:05:44 +0800

    [mod] update

--- controller/index.ts
+++ controller/index.ts
@@ -1,9 +1,9 @@
 /*
  * @FilePath        : /node-space/controller/index.ts
  * @Author          : zlq midhuhu@163.com
- * @Description:    :
+ * @Description:    : 控制器导出
  * @Date            : 2024-03-26 08:16:50
- * @LastEditTime    : 2024-03-28 16:44:16
+ * @LastEditTime    : 2024-04-03 11:05:23
  * @Copyright (c) 2024 by zhijiasoft.
  */
 
